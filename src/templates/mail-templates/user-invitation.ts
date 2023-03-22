29814aebaa1b7cf8efdf85b5ed3bf39acd38e299
Leshkinson <lopatkool93@gmail.com>
2023-03-22 22:34:16 +0300

    fix url confirm

--- src/templates/mail-templates/user-invitation.ts
+++ src/templates/mail-templates/user-invitation.ts
@@ -7,7 +7,7 @@ export function userInvitationTemplate(mail: string, code: string): UserInvitati
     return {
         to: mail,
         html: `
-        <h1>Thank for your registration</h1>
+        <h1>Thank for your registration!</h1>
     <p>To finish registration please follow the link below:
         <a href='https://h07-api-v1.vercel.app/auth/registration-email-resending?code=${code}'>complete registration</a>
     </p>`
